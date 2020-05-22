a4dfcabfec39b3b7fddc463ec00424e270b30ffd
joseiscj <jose.ivan.junior@ccc.ufcg.edu.br>
2020-05-22 19:10:32 -0300

    Tratamento de erro e construção do loading

--- src/main.js
+++ src/main.js
@@ -16,6 +16,18 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
+    setLoading(loading = true) {
+        if (loading === true) {
+            let loadingEl = document.createElement("span");
+            loadingEl.appendChild(document.createTextNode("Carregando..."));
+            loadingEl.setAttribute("id", "loading");
+
+            this.formEl.appendChild(loadingEl);
+        } else {
+            document.getElementById("loading").remove();
+        }
+    }
+
     async addRepository(event) {
         event.preventDefault();
 
@@ -24,23 +36,31 @@ class App {
 
         if (repoInput.length === 0) 
             return;
+
+        this.setLoading();
         
-        var response = await axios.get(`https://api.github.com/repos/${repoInput}`);
+        try {
+            const response = await axios.get(`https://api.github.com/repos/${repoInput}`);
 
-        console.log(response.data);
+            console.log(response.data);
 
-        const { name, description, html_url, owner: {avatar_url} } = response.data;
+            const { name, description, html_url, owner: {avatar_url} } = response.data;
 
-        this.repositories.push({
-            name,
-            description,
-            avatar_url,
-            html_url,
-        });
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
 
-        this.inputEl.value = "";
+            this.inputEl.value = "";
 
-        this.render();
+            this.render();
+        } catch (err) {
+            alert("O reposiório não existe!")
+        }
+
+        this.setLoading(false);
     }
 
     render() {
