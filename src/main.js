c0bbfdd46405d6b3a9751935e924dfe8b943a4c2
joseiscj <jose.ivan.junior@ccc.ufcg.edu.br>
2020-05-21 22:02:10 -0300

    criaçao da função de renderização da lista pelo JavaScript

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
         this.repositories = [];
 
         this.formEl = document.getElementById("repo-form");
+        this.listEl = document.getElementById("repo-list");
 
         this.registerHandlers();
     }
@@ -19,7 +20,36 @@ class App {
             description: "",
             avatar_url: "",
             html_url: ""
-        })
+        });
+
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = "";
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement("img");
+            imgEl.setAttribute("src", repo.avatar_url);
+
+            let titleEl = document.createElement("strong");
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let description = document.createElement("p");
+            description.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement("a");
+            linkEl.setAttribute("target", "_blank");
+            linkEl.appendChild(document.createTextNode("Acessar"));
+
+            let listItemEl = document.createElement("li");
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(description);
+            listItemEl.appendChild(linkEl);
+            
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
